b460ed7a8434f2b3688cdca47307447c9ec0d0ab
keverson-tds <keverson.tds2020@gmail.com>
2020-10-24 18:12:10 -0400

    *remove arquivo config;

--- _connect/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-date_default_timezone_set('America/Cuiaba');
-define('DB_HOSTNAME', '10.52.30.86');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '@Sfiemt30#');
-define('DB_DATABASE', 'cms_sfiemt_dev');
-define('DB_CHARSET', 'UTF8');
