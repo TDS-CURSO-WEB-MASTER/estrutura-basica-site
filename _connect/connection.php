0848126131e6acd9a82498e91ca21691837322c8
keverson-tds <keverson.tds2020@gmail.com>
2020-10-24 17:25:59 -0400

    *criando conexão com o banco de dados mysql;

--- _connect/connection.php
+++ _connect/connection.php
@@ -2,3 +2,35 @@
 	
 require_once __DIR__.'./config.php';
 
+//Proteção contra SQLInjection
+function DBEscape($dados){
+    $conn = DBConnect();
+    
+    if(!is_array($dados))
+        $dados = mysqli_real_escape_string($conn, $dados);
+    else{
+        $arr = $dados;
+        foreach ($arr as $key => $value) {
+            $key = mysqli_real_escape_string($conn, $key);
+            $value = mysqli_real_escape_string($conn, $value);
+
+            $dados[$key] = $value;
+        }
+    }
+
+    DBClose($conn);
+    return $dados;
+}
+
+//Fecha conexão com MySQL
+function DBClose($conn){
+@mysqli_close($conn) or die(mysqli_error($conn));
+}
+
+//Abre conexão com MySQL
+function DBConnect(){
+    $conn = @mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die(mysqli_connect_error());
+    mysqli_set_charset($conn, DB_CHARSET) or die(mysqli_error($conn));
+    
+    return $conn;
+}
--- _connect/database.php
+++ _connect/database.php
@@ -3,3 +3,66 @@
 require_once __DIR__.'/connection.php';
 require_once __DIR__.'/../functions/functions.php';
 
+//Deleta registros
+function DBDelete($table, $where = null){
+    $where = ($where) ? " WHERE {$where}" : null;
+
+    $query = "DELETE FROM {$table}{$where}";
+
+    DBExecute($query);
+}
+
+//Altera registros
+function DBUpdate($table, array $data, $where = null){
+    $data 	= DBEscape($data);
+    foreach ($data as $key => $value) {
+        $fields[] = "{$key} = '{$value}'";
+    }
+
+    $fields = implode(', ', $fields);
+
+    $where = ($where) ? " WHERE {$where}" : null;
+
+    $query = "UPDATE {$table} SET {$fields}{$where}";
+
+    return DBExecute($query);
+}
+
+//Lê registros no banco de dados
+function DBSelect($table, $params = null, $fields = '*'){
+
+    $params = $params ? " {$params}" : null;
+    $query = "SELECT {$fields} FROM {$table}{$params}";
+    
+    $result = DBExecute($query);
+
+    if(mysqli_num_rows($result)){
+        while ($res = mysqli_fetch_assoc($result)) {
+            $data[] = $res;
+        }
+        return $data;
+    }
+
+    return 'A consulta não retornou nenhum resultado.';
+}
+	
+//Insere no banco de dados
+function DBCreate($table, array $data){
+    $data 	= DBEscape($data);
+    $fields	= implode(', ', array_keys($data));
+    $values = "'".implode("', '", $data)."'";
+
+    $query = "INSERT INTO {$table} ( {$fields} ) VALUES ( {$values} )";
+
+    return DBExecute($query);
+}
+
+//Executa Queries
+function DBExecute($query){
+    
+    $link 	= DBConnect();
+    $result = @mysqli_query($link, $query) or die(mysqli_error($link));
+    
+    DBClose($link);
+    return $result;
+}
